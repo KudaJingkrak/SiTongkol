5f9d3fc72236288553f9fd1e09307c9075ba6f05
Muhammad Ardhan <muhammad.ardhan@live.com>
2018-01-11 17:58:26 +0700

    Adding DOTween Directory and also stuff

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class DialogueManager : MonoBehaviour {
 
+	public Image UI_Controller;
 	public Text nameText;
 	public Text dialogueText;
 	private Queue<string> sentences;
+	public Vector3 Posisi_Depan;
+	public Vector3 Posisi_Belakang;
+
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +25,8 @@ public class DialogueManager : MonoBehaviour {
 	}
 	public void StartDialogue(Dialogue dialogue)
 	{
+		UI_Controller.rectTransform.DOLocalMoveY(Posisi_Depan.y,0.5f);
+		StartCoroutine(wait(0.5f));
 		//TODO : Nanti disini akan nge load Dialogue apa yang akan dilakukan, dengan apa ?
 		nameText.text = dialogue.name;
 		sentences.Clear();
@@ -38,12 +45,27 @@ public class DialogueManager : MonoBehaviour {
 			return;
 		}
 		string sentence = sentences.Dequeue();
-		dialogueText.text = sentence;
-
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+	IEnumerator wait(float second)
+	{
+		yield return new WaitForSeconds(second);
+	}
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogueText.text = "";
+		foreach(char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return new WaitForSeconds(0.025f);
+		}
 	}
 
 	void EndDialogue()
 	{
-		dialogueText.text = "End of Conversation";
+		UI_Controller.rectTransform.DOLocalMoveY(Posisi_Belakang.y,0.5f);
+		dialogueText.text = "";
+		nameText.text = "";
 	}
 }
