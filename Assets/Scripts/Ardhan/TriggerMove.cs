f841adda4501fa195b17f0854158649956d3435d
Muhammad Ardhan <muhammad.ardhan@live.com>
2018-01-13 20:14:48 +0700

    Pulling Object Prototype

--- Assets/Scripts/Ardhan/TriggerMove.cs
+++ Assets/Scripts/Ardhan/TriggerMove.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class TriggerMove : MonoBehaviour {
     public bool inArea;
+    public Rigidbody2D parent_Rigidbody;
 	// Use this for initialization
 	void Start () {
-		
+        parent_Rigidbody.isKinematic = false;
 	}
 	
 	// Update is called once per frame
@@ -19,8 +20,29 @@ public class TriggerMove : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             inArea = true;
+                
         }
     }
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(Input.GetButtonDown("X"))
+            {
+                transform.parent.SetParent(other.transform);
+                parent_Rigidbody.isKinematic = true;
+                other.gameObject.GetComponent<Rigidbody2D>().constraints = transform.parent.gameObject.GetComponent<Rigidbody2D>().constraints;
+            }
+            else if(Input.GetButtonUp("X"))
+            {   
+                 transform.parent.SetParent(null);
+                 parent_Rigidbody.isKinematic = false;
+                 other.gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                 
+            }
+        }
+    }
+
 
     public void OnTriggerExit2D(Collider2D other)
     {
