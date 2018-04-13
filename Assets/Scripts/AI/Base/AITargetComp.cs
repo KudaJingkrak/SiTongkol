35b9c0daa0f84129373b198f967d570325fdbe58
Arif Budiman <arif.budiman41@ui.ac.id>
2018-04-13 10:13:20 +0700

    Update AI Component
    Add 4 direction quick movement
    Add Object Aiming for AI target
    Add Offset projectile for spwan transform projectile
    Add firing method for AI target
    Add Auto Aiming target for AI target

--- Assets/Scripts/AI/Base/AITargetComp.cs
+++ Assets/Scripts/AI/Base/AITargetComp.cs
@@ -5,8 +5,15 @@ using Panda;
 
 [RequireComponent(typeof(AIMovementComp))]
 public class AITargetComp : MonoBehaviour {
-	public GameObject target;
-	private AIMovementComp _movement;
+	
+	[Panda.Task]
+	public bool isCanFire{get{return fireRateCounter <= 0;}}	
+	public GameObject objectAiming;
+	public Transform[] offsetProjectile;
+	public float turnRate = 30f;
+	public float fireRate = 1f;
+	private float fireRateCounter, tempFireRate = -1f, tempFireCacl;
+	private AIMovementComp _movementComp;
 	
 	[Header("Projectile")]
 	public GameObject prefab;
@@ -16,15 +23,55 @@ public class AITargetComp : MonoBehaviour {
 		Fire(transform.position, transform.rotation);
 		Panda.Task.current.Succeed();
 	}
+
+	[Panda.Task]
+	public void FireWithOffset(){
+		if(offsetProjectile.Length > 0){
+			for(int i = 0; i < offsetProjectile.Length; i++){
+				Fire(offsetProjectile[i].position, offsetProjectile[i].rotation);
+			}
+			Panda.Task.current.Succeed();
+		}else{
+			Panda.Task.current.Fail();
+		}
+	}
+
+	[Panda.Task]
+	public void FireWithOffset(int index){
+		if(offsetProjectile.Length > 0 && index < offsetProjectile.Length){
+			Fire(offsetProjectile[index].position, offsetProjectile[index].rotation);
+			Panda.Task.current.Succeed();
+		}else{
+			Panda.Task.current.Fail();
+		}
+	}
+
+	[Panda.Task]
+	public void FireWithRandomOffset(){
+		int index = Random.Range(0, offsetProjectile.Length);
+		if(offsetProjectile.Length > 0 && index < offsetProjectile.Length){
+			Fire(offsetProjectile[index].position, offsetProjectile[index].rotation);
+			Panda.Task.current.Succeed();
+		}else{
+			Panda.Task.current.Fail();
+		}
+	}
+
+	[Panda.Task]
+	public void FireDirection(float x, float y){
+		FireDirection(new Vector2(x,y));
+		Panda.Task.current.Succeed();
+	}
 	
 	public void Fire(Vector2 position, Quaternion rotation){
+		CalcFireRate();
 		PoolManager.Instance.ReuseObject(prefab, position, rotation);	
 	}
 
 	public void FireDirection(Vector2 position){
 		Quaternion rotation;
 
-		switch(_movement.GetDirection()){
+		switch(_movementComp.GetDirection()){
 			case Direction.Back:
 				rotation = Quaternion.Euler(0f,0f,0f);
 				break;
@@ -41,18 +88,56 @@ public class AITargetComp : MonoBehaviour {
 				rotation = Quaternion.Euler(0f,0f,180f);
 				break;
 		}
+		CalcFireRate();
 		PoolManager.Instance.ReuseObject(prefab, position, rotation);
 	}
 
-	[Panda.Task]
-	public void FireDirection(float x, float y){
-		FireDirection(new Vector2(x,y));
-		Panda.Task.current.Succeed();
+	public void CalcFireRate(){
+		if(tempFireRate != fireRate){
+			tempFireRate = fireRate;
+			tempFireCacl = 1/fireRate;
+		}
+		fireRateCounter = tempFireCacl;
+	}
+
+	public void AimTarget(){
+		if(objectAiming && _movementComp.target){
+			Quaternion newRotation  = Quaternion.LookRotation(objectAiming.transform.position - _movementComp.target.position, Vector3.forward);
+			newRotation.x = objectAiming.transform.rotation.x;
+			newRotation.y = objectAiming.transform.rotation.y;
+			objectAiming.transform.rotation = Quaternion.Slerp(objectAiming.transform.rotation, newRotation, Time.deltaTime * turnRate );
+		}
+	}
+
+	public void AimTurnClockwise(float speed = 10f){
+		objectAiming.transform.Rotate(Vector3.back * speed * turnRate * Time.deltaTime);
+	}
+
+	public void AimTurnCounterClockwise(float speed = 10f){
+		objectAiming.transform.Rotate(Vector3.forward * speed * turnRate * Time.deltaTime);
+	}
+
+	public void SetAimRotation(float angle){
+		objectAiming.transform.Rotate(0,0,angle);
 	}
 
 	// Use this for initialization
 	void Start () {
-		_movement = GetComponent<AIMovementComp>();
+		_movementComp = GetComponent<AIMovementComp>();
+		CalcFireRate();
+	}
+
+	void Update(){
+		
+		if(isCanFire){
+			//SetAimRotation(Random.Range(0,360));
+			for(int i = 0; i < offsetProjectile.Length; i++){
+				Fire(offsetProjectile[i].position, offsetProjectile[i].rotation);
+			}
+		}
+		if(fireRateCounter > 0 ){
+			fireRateCounter -= Time.deltaTime;
+		}
 	}
 	
 }
