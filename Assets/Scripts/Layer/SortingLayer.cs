81fe91530e34134789f09571c432b84196923b5b
Arif Budiman <arif.budiman41@ui.ac.id>
2018-05-04 11:12:16 +0700

    Update Layer
    Update layer, dipisah jadi background, midground, character, forground

---
+++ Assets/Scripts/Layer/SortingLayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingLayer : MonoBehaviour {
+
+	SpriteRenderer tempRenderer;
+	
+	void Awake(){
+		tempRenderer = GetComponentInParent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		tempRenderer.sortingOrder = (int) Camera.main.WorldToScreenPoint(tempRenderer.bounds.min).y * -1;
+	}
+}
