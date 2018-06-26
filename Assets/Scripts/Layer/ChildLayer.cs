386c2cffea08b0e129c415b81c96bf23cd763b6c
Arif Budiman <arif.budiman41@ui.ac.id>
2018-06-26 15:26:44 +0700

    Ilangin layer character diganti sama default

---
+++ Assets/Scripts/Layer/ChildLayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildLayer : MonoBehaviour {
+	SpriteRenderer parentRender;
+    SpriteRenderer myRender;
+
+    void Awake()
+    {
+        parentRender = GetComponentInParent<SpriteRenderer>();
+        myRender = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        myRender.sortingOrder = parentRender.sortingOrder;  
+    }
+}
