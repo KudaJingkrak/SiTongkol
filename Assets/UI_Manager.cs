61000fa09a3acb3ba9f7d455dc4a1959d585fabd
Unknown <dandut88@gmail.com>
2018-07-17 23:11:40 +0700

    Update UI Manager Issue Fixed
    Done

---
+++ Assets/UI_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour {
+    public Canvas canvas_Player; 
+
+	// Use this for initialization
+	void Start () {
+        canvas_Player = this.GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (canvas_Player.worldCamera == null)
+        {
+            canvas_Player.worldCamera = Camera.main;
+        }
+	}
+}
